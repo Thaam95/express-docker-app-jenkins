689168cfa3db265abc072902d120b00f3e6ad6c0
ThaamAnubiz <thaampapatt@gmail.com>
2025-11-16 19:43:35 +0700

    Test Jenkins automation

--- src/app.ts
+++ src/app.ts
@@ -26,5 +26,14 @@ app.get('/api/health', (_: Request, res: Response) => {
   })
 })
 
+// GET /api/users
+app.get('/api/users', (_: Request, res: Response) => {
+  const users = [
+    { id: 1, name: 'Thaam T.' },
+    { id: 2, name: 'Shermin M.' }
+  ]
+  res.json(users)
+})
+
 // Start server
 app.listen(port, () => console.log(`Application is running on port ${port}`))
